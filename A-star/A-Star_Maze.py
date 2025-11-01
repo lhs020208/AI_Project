e627c8b912febb778cd5a3b75313d05d41dd1308
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-11-02 01:10:00 +0900

    노드 상태 설정

--- A-star/A-Star_Maze.py
+++ A-star/A-Star_Maze.py
@@ -16,7 +16,13 @@ class Node:
         self.x = x
         self.y = y
         self.state = state
+        self.g = float('inf')
+        self.h = 0
+        self.f = float('inf')
+        self.parent = None
 
+def manhattan(a, b):
+    return abs(a.x - b.x) + abs(a.y - b.y)
 
 # 초기화
 open_canvas(400, 400)
@@ -32,6 +38,15 @@ Purple_node_img = load_image("PurpleNode.png")
 
 # 20x20 그리드 생성
 grid = [[Node(i, j, 0) for j in range(20)] for i in range(20)]
+
+# 마우스 상태 변수
+left_pressed = False
+right_pressed = False
+LeftClickNum = 0
+
+# A* 알고리즘을 위한 오픈 리스트와 클로즈드 리스트
+open_list = []
+closed_list = set()
 
 # 메인 루프
 running = True
@@ -61,9 +76,73 @@ while running:
 
     update_canvas()
 
-    # 이벤트 처리
+    # --- 이벤트 처리 ---
     for e in get_events():
+        # 종료
         if e.type == SDL_QUIT or (e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE):
             running = False
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_a:
+            for i in range(20):
+                for j in range(20):
+                    node = grid[i][j]
+                    if node.state == 10 or node.state == 20:
+                        node.state = 0
+            LeftClickNum = 0
+
+        # 좌클릭 시작 (누름)
+        elif e.type == SDL_MOUSEBUTTONDOWN and e.button == SDL_BUTTON_LEFT:
+            numx = e.x // 20
+            numy = 20 - (e.y // 20) - 1
+            if not (0 <= numx < 20 and 0 <= numy < 20):
+                continue
+            node = grid[numx][numy]
+
+            if LeftClickNum == 0 and node.state == 0:
+                node.state = 10  # Start
+                LeftClickNum = 1
+            elif LeftClickNum == 1 and node.state == 0:
+                node.state = 20  # Goal
+                LeftClickNum = 2
+            elif LeftClickNum == 2:
+                left_pressed = True
+                if node.state == 0:
+                    node.state = 1  # 벽 생성
+
+        # 좌클릭 드래그 (벽 그리기)
+        elif e.type == SDL_MOUSEMOTION and left_pressed and LeftClickNum == 2:
+            numx = e.x // 20
+            numy = 20 - (e.y // 20) - 1
+            if 0 <= numx < 20 and 0 <= numy < 20:
+                node = grid[numx][numy]
+                if node.state == 0:
+                    node.state = 1
+
+        # 좌클릭 해제
+        elif e.type == SDL_MOUSEBUTTONUP and e.button == SDL_BUTTON_LEFT:
+            left_pressed = False
+
+        # 우클릭 (지우기)
+        elif e.type == SDL_MOUSEBUTTONDOWN and e.button == SDL_BUTTON_RIGHT:
+            right_pressed = True
+            numx = e.x // 20
+            numy = 20 - (e.y // 20) - 1
+            if not (0 <= numx < 20 and 0 <= numy < 20):
+                continue
+            node = grid[numx][numy]
+            if node.state == 1:
+                node.state = 0
+
+        # 우클릭 드래그 (연속 지우기)
+        elif e.type == SDL_MOUSEMOTION and right_pressed:
+            numx = e.x // 20
+            numy = 20 - (e.y // 20) - 1
+            if 0 <= numx < 20 and 0 <= numy < 20:
+                node = grid[numx][numy]
+                if node.state == 1:
+                    node.state = 0
+
+        # 우클릭 해제
+        elif e.type == SDL_MOUSEBUTTONUP and e.button == SDL_BUTTON_RIGHT:
+            right_pressed = False
 
 close_canvas()
